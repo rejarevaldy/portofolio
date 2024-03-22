e128a05f28f18d2d29bda9c3fbe3ff6c0275b9a0
rejarevaldy <rejarevaldy30@gmail.com>
2024-03-22 21:31:28 +0800

    Change output to "server" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "hybrid",
+  output: "server",
   adapter: vercel(),
 });
