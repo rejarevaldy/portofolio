480a6642afc733dd10d07cbef4a6d4347a4f36d5
rejarevaldy <rejarevaldy30@gmail.com>
2024-03-22 20:34:27 +0800

    Update astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
@@ -7,6 +7,6 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
+  adapter: vercel(),
 });
